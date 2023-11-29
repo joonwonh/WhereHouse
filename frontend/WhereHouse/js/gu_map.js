8a577d1c415605c9c837d64b540b80edd3d6256c
joonwonh <joonwon24@gmail.com>
2023-11-29 14:21:21 +0900

    feat: 메인화면 사이드바 추가

--- frontend/WhereHouse/js/gu_map.js
+++ frontend/WhereHouse/js/gu_map.js
@@ -1,6 +1,4 @@
 window.onload = function () {
-    console.log("hi");
-    var tmp = "ss";
     var container = document.getElementById("map");
     var options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
