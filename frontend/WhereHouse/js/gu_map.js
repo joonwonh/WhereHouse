4dc3f062cfd27c654aba3a4bfeeaf32d27c57d3b
beudicri <ys4512558@naver.com>
2023-11-29 14:04:26 +0900

    feat: iframe 패널과 카카오지도 화면구성

---
+++ frontend/WhereHouse/js/gu_map.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    console.log("hi");
+    var tmp = "ss";
+    var container = document.getElementById("map");
+    var options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+    };
+
+    var map = new kakao.maps.Map(container, options);
+
+    //패널 열고 닫기
+    var info = document.querySelector("#information");
+    var func = document.querySelector("#btn");
+
+    func.addEventListener("click", panelFunc);
+
+    function panelFunc() {
+        if (info.style.left == "0px") {
+            info.style.left = "-333px";
+            func.innerText = "▶";
+        } else {
+            info.style.left = "0px";
+            func.innerText = "◀";
+        }
+    }
+}
