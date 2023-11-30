7c84f5e64b42bbcafbb4f357d31f88ec5fc06b7c
WangSeungCheol <ys4512558@naver.com>
2023-11-30 16:10:20 +0900

    feat: 행정동 삭제 및 팝업 창 띄우기 기능 추가

--- frontend/WhereHouse/js/main.js
+++ frontend/WhereHouse/js/main.js
@@ -1,33 +1,29 @@
 window.onload = function () {
-    var menu_gu_icon = document.getElementById("menu_gu_icon");
-    var menu_dong_icon = document.getElementById("menu_dong_icon");
-    var menu_detail_icon = document.getElementById("menu_detail_icon");
     var menu_suggest_icon = document.getElementById("menu_suggest_icon");
+    var menu_gu_icon = document.getElementById("menu_gu_icon");
+    var menu_detail_icon = document.getElementById("menu_detail_icon");
+
     var kakaoMap = document.getElementById("kakao_map");
 
-    menu_gu_icon.addEventListener("click", () => clickMenu(1));
-    menu_dong_icon.addEventListener("click", () => clickMenu(2));
+    menu_suggest_icon.addEventListener("click", () => clickMenu(1));
+    menu_gu_icon.addEventListener("click", () => clickMenu(2));
     menu_detail_icon.addEventListener("click", () => clickMenu(3));
-    menu_suggest_icon.addEventListener("click", () => clickMenu(4));
 
     function clickMenu(sel) {
         menu_gu_icon.style.backgroundColor = "rgba(11, 94, 215, 1)";
-        menu_dong_icon.style.backgroundColor = "rgba(11, 94, 215, 1)";
         menu_detail_icon.style.backgroundColor = "rgba(11, 94, 215, 1)";
         menu_suggest_icon.style.backgroundColor = "rgba(11, 94, 215, 1)";
 
         if (sel === 1) {
-            menu_gu_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
-            kakaoMap.src = "pages/main.html";
+            menu_suggest_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
+            //추천 iframe 로직
+            // kakaoMap.src = "pages/main.html";
         } else if (sel === 2) {
-            menu_dong_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
-            kakaoMap.src = "pages/map.html";
+            menu_gu_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
+            //구 iframe 로직
         } else if (sel === 3) {
             menu_detail_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
-            //상세 지도 ifram 변경 로직 추가
-        } else if (sel === 4) {
-            menu_suggest_icon.style.backgroundColor = "rgba(34, 34, 34, 0.3)";
-            //주거지 추천 ifram 변경 로직 추가
+            //상세 지도 ifram 로직
         }
     }
 }
