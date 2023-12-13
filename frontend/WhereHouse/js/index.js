07805f0dfb81d8e3e03058b327650b1d7e780eb6
joonwonh <joonwon24@gmail.com>
2023-12-13 17:25:22 +0900

    feat: 메인 페이지 및 분석 설명 페이지 디자인 변경

--- frontend/WhereHouse/js/index.js
+++ frontend/WhereHouse/js/index.js
@@ -14,6 +14,7 @@ window.onload = function () {
   observer.observe(img[3]);
   observer.observe(img[4]);
 
+
   // 이미지 아래서 위로
   let observers = new IntersectionObserver((e) => {
     e.forEach((content) => {
@@ -22,12 +23,13 @@ window.onload = function () {
       }
     })
   })
-  let img_content = document.querySelectorAll(".img_content");
+  let img_content = document.querySelectorAll(".img_content img");
   observers.observe(img_content[0]);  // html요소 감시
   observers.observe(img_content[1]);
   observers.observe(img_content[2]);
   observers.observe(img_content[3]);
   observers.observe(img_content[4]);
+
 
   // 텍스트 opacity
   let observerse = new IntersectionObserver((e) => {
@@ -43,6 +45,7 @@ window.onload = function () {
   observerse.observe(site_page_innerText[2]);
   observerse.observe(site_page_innerText[3]);
   observerse.observe(site_page_innerText[4]);
+
 
   // 상단 네비게이션 바 선택에 필요한 a 태그
   var btn_gu_map = document.getElementById("nav_btn_gu_map");
